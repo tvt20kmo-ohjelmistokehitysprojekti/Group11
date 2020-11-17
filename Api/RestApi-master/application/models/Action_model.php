0a5c7fb97b515a224061c951475bdedd39baf72a
Tuukka <t9satu01@students.oamk.fi>
2020-11-17 14:03:26 +0200

    RestApi valmis

---
+++ Api/RestApi-master/application/models/Action_model.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ *
+ */
+class Action_model extends CI_model
+{
+  function get_action($id){
+    $this->db->select('*');
+    $this->db->from('action');
+    if($id !== NULL) {
+      $this->db->where('idaction',$id);
+    }
+    return $this->db->get()->result_array();
+  }
+  function add_action($add_data){
+    $this->db->insert('action',$add_data);
+    if($this->db->insert_id()!==NULL){
+      return $this->db->insert_id(); 
+    }
+    else {
+      return FALSE;
+    }  
+  }
+  function update_action($id, $update_data){
+    $this->db->where('idaction',$id);
+    $this->db->update('action',$update_data);
+    if($this->db->affected_rows()>0){
+      return TRUE; 
+    }
+    else {
+      return FALSE;
+    }
+  }
+
+  function delete_action($id){
+    $this->db->where('idaction',$id);
+    $this->db->delete('action');
+    if($this->db->affected_rows()>0){
+      return TRUE; 
+    }
+    else {
+      return FALSE;
+    }
+  }
+
+
+}
---
+++ Api/RestApi-master/application/models/Card_model.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ *
+ */
+class Card_model extends CI_model
+{
+  function get_card($id){
+    $this->db->select('*');
+    $this->db->from('card');
+    if($id !== NULL) {
+      $this->db->where('idcard',$id);
+    }
+    return $this->db->get()->result_array();
+  }
+  function add_card($add_data){
+    $this->db->insert('card',$add_data);
+    if($this->db->insert_id()!==NULL){
+      return $this->db->insert_id(); 
+    }
+    else {
+      return FALSE;
+    }  
+  }
+  function update_card($id, $update_data){
+    $this->db->where('idcard',$id);
+    $this->db->update('card',$update_data);
+    if($this->db->affected_rows()>0){
+      return TRUE; 
+    }
+    else {
+      return FALSE;
+    }
+  }
+
+  function delete_card($id){
+    $this->db->where('idcard',$id);
+    $this->db->delete('card');
+    if($this->db->affected_rows()>0){
+      return TRUE; 
+    }
+    else {
+      return FALSE;
+    }
+  }
+
+
+}
